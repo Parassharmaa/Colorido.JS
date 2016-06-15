15d74696fdf94d688f4a758a2b087e0cd5b4d8d3
parassharmaa <paraaZz@outook.com>
2016-06-15 18:06:25 +0530

    Scripts updated

--- colorido.js
+++ colorido.js
@@ -1,15 +1,52 @@
-target1 = document.getElementsByClassName("colorido"), target2 = document.getElementsByClassName("colorido-o"), target3 = document.getElementsByClassName("colorido-t"), max = config.choice.length, time = 1e3 * config.autoDuration, trans = 1 - config.opacity, animD = " " + config.animationDuration + "s ", animT = config.animationType, config.autoChange ? (renc(), setInterval(renc, time)) : renc(), config.chromeTab && (metaIn("theme-color"), metaIn("msapplication-navbutton-color"), metaIn("apple-mobile-web-app-status-bar-style"));
-
+target1 = document.getElementsByClassName('colorido');
+target2 = document.getElementsByClassName('colorido-o');
+target3 = document.getElementsByClassName('colorido-t');
+max = config.choice.length;
+time = config.autoDuration * 1000;
+trans = 1-config.opacity;
+animD = " "+config.animationDuration+"s"+" ";
+animT = config.animationType;
+	
 function renc() {
-	var t = parseInt(Math.random() * (max - 0) + 0);
-	color = config.choice[t], conv = color.replace("b", "ba"), trans_color = conv.replace(")", "," + trans);
-	var e;
-	for (e = 0; e < target1.length; e++) target1[e].style.backgroundColor = color, target1[e].style.color = "#fff", target1[e].style.transition = "background-color" + animD + animT;
-	for (e = 0; e < target2.length; e++) target2[e].style.backgroundColor = trans_color, target2[e].style.transition = "background-color" + animD + animT;
-	for (e = 0; e < target3.length; e++) target3[e].style.color = color, target3[e].style.transition = "color" + animD + animT
+	rand = parseInt(Math.random() * (max - 0) + 0);
+	color = config.choice[rand];
+	conv = color.replace('b', 'ba');
+	trans_color = conv.replace(')', "," + trans);
+	var i;
+	
+	for (i = 0; i < target1.length; i++) {
+		target1[i].style.backgroundColor = color;
+		target1[i].style.color = "#fff";
+		target1[i].style.transition = "background-color"+animD + animT;
+	}
+
+	for (i = 0; i < target2.length; i++) {
+		target2[i].style.backgroundColor = trans_color;
+		target2[i].style.transition = "background-color"+animD+ animT;
+	}
+
+	for (i = 0; i < target3.length; i++) {
+		target3[i].style.color = color;
+		target3[i].style.transition = "color"+animD+ animT;
+	}
+
+};
+if (config.autoChange) {
+	renc();
+	setInterval(renc, time);
+} else {
+	renc();
 }
 
-function metaIn(t) {
-	var e = document.createElement("meta");
-	e.name = t, e.content = config.choice[rand], document.getElementsByTagName("head")[0].appendChild(e)
+function metaIn(mdevice) {
+	var meta = document.createElement('meta');
+	meta.name = mdevice;
+	meta.content = config.choice[rand];
+	document.getElementsByTagName('head')[0].appendChild(meta);
+
+}
+if (config.chromeTab) {
+	metaIn("theme-color");
+	metaIn("msapplication-navbutton-color");
+	metaIn("apple-mobile-web-app-status-bar-style");
 }
